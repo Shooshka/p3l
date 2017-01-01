2c3d20d89cdfc3ba8adf901b58f84fd0b3933a2d
Shoo <root@ishoo.ru>
2017-01-01 12:49:31 +0300

    othet

--- otchet.py
+++ otchet.py
@@ -2,21 +2,23 @@ import datetime
 import sys
 
 if len(sys.argv) == 1:
-    print("Usage: {} new or {} count".format(sys.argv[0], sys.argv[0]))
-elif sys.argv[1] == 'new':
-    now = datetime.datetime.now()
-    today = str(now.day) + '.' + str(now.month) + '.' + str(now.year)
-    client = input('Клиент: ')
-    bases = input('Сколько баз? ')
-    ticket = input('Номер тикета')
-    with open('otchet.txt', 'a') as ouf:
-        print(today, client, bases, str(ticket), sep=',', file=ouf)
-    print('Успешно внесли в список обнов')
+    print('ERROR! You must specify at least one option')
+else:
+    param_name = sys.argv[1]
+    if param_name == 'new':
+        now = datetime.datetime.now()
+        today = str(now.day) + '.' + str(now.month) + '.' + str(now.year)
+        client = input('Клиент: ')
+        bases = input('Сколько баз? ')
+        ticket = input('Номер тикета: ')
+        with open('otchet.txt', 'a') as ouf:
+            print(today, client, bases, str(ticket), sep=',', file=ouf)
+        print('Успешно внесли в список обнов')
 
-elif sys.argv[1] == 'count':
-    s = []
-    with open('otchet.txt', 'r') as inf:
-        for line in inf:
-            line = line.strip()
-            s.append(line.split(','))
-    print('А всего на данный момент обновлено ' + str(sum([int(bases) for today, client, bases, ticket in s])) + ' баз')
+    elif param_name == 'count':
+        s = []
+        with open('otchet.txt', 'r') as inf:
+            for line in inf:
+                line = line.strip()
+                s.append(line.split(','))
+        print('А всего на данный момент обновлено ' + str(sum([int(bases) for today, client, bases, ticket in s])) + ' баз')
