bbc3e9bf34b550563645afdce5ce1b357a70bd1b
Shoo <root@ishoo.ru>
2017-01-24 17:36:27 +0300

    v2

--- passgen.py
+++ passgen.py
@@ -14,10 +14,10 @@ def gen(event):
     pyperclip.copy(password)
 
 root = Tk()
-btn = Button(root,                  #родительское окно
-             text="Сгенерировать",       #надпись на кнопке
-             width=30,height=5,     #ширина и высота
-             bg="white",fg="black") #цвет фона и надписи
-btn.bind("<Button-1>", gen)       #при нажатии ЛКМ на кнопку вызывается функция gen
-btn.pack()                          #расположить кнопку на главном окне
+btn = Button(root,
+             text="Сгенерировать",
+             width=30,height=5,
+             bg="white",fg="black")
+btn.bind("<Button-1>", gen)
+btn.pack()
 root.mainloop()
